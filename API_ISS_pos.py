2b86bb515d738f92e6f0128b197a170244ab9d1e
Soenke <soenkemaibach@gmail.com>
2021-07-19 14:31:34 +0200

    unique ID for every location, amount of localizations minimized, better access to address data prepared

--- API_ISS_pos.py
+++
@@ -1,6 +0,0 @@
-import requests
-res = requests.get('http://api.open-notify.org/iss-now.json')
-if res:
-    print('Latitude: '+ res.json()['iss_position']['latitude'])
-else:
-    print('Response Failed')
