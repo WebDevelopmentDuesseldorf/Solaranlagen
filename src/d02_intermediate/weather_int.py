1d8e1529e8187668a5a6b99f6f50537c944b208b
Soenke <soenkemaibach@gmail.com>
2021-08-14 11:06:57 +0200

    added func to estimate thickness of snow on panel

--- src/d02_intermediate/weather_int.py
+++ src/d02_intermediate/weather_int.py
@@ -7,3 +7,13 @@ def est_paneltemp(air_temp, wind_speed, ventilated=True):
     wind_cooling = wind_speed/2+.0625*wind_speed**2
     panel_temp = air_temp+40- wind_cooling
     return panel_temp
+
+def est_snowthickness(snow_depth):
+    '''
+    returns estimation of snow thickness on the solar panel
+    :param snow_depth: current snow depth at given location
+    '''
+    est = snow_depth-5
+    if est < 0:
+        est =0
+    return est
