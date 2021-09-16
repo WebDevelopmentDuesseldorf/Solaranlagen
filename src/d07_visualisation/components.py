0b699d365dde0883caf1e3632502dd3a5f2a95c9
Soenke <soenkemaibach@gmail.com>
2021-09-17 01:05:35 +0200

    custom components for the dashboard

--- src/d07_visualisation/components.py
+++ src/d07_visualisation/components.py
@@ -8,9 +8,8 @@ start_button = html.Div(
     [
         dbc.Button(
             "Start the analysis!", id="start_button",
-            className="mr-2", n_clicks=0, color='info'
+            className="m-2", n_clicks=0, color='info'
         ),
-        html.Span(id="example-output", style={"verticalAlign": "middle"}, children='fix the start button'),
     ]
 )
 
@@ -60,3 +59,30 @@ your_fig = {
         ]
     }
 }
+
+description =  dbc.CardBody([
+    html.Div('Find the perfect spot for generating solar power',style={'font-weight': 'bold'}),
+    html.Div('''
+    Welcome! You can use this site to analyse different 
+    locations and find out, if they are suited for generating solar power. 
+    This site will show you the optimal spots for high energy output or a maximum
+    return on your investment.
+    '''),
+    html.Div('Get your personalized analysis',style={'font-weight': 'bold'}),
+    html.Div('''
+    Input your info and you will receive an analysis report tailored to your needs.
+    '''),
+    html.Div('About the inputs',style={'font-weight': 'bold'}),
+    html.Div('''
+    You can input three different types of information for now. The first one
+    is the location you are interested in. This works with cities, regions or even 
+    whole countries. The second one is the resolution of the map. Lower resolution are a 
+    lot quicker but you can choose a higher resolution if you are interested in detailed
+    data. The last input is the household size. Different household sizes lead to different 
+    electricity needs. This will be taken into account for the analysis.
+    '''),
+    html.Div('''
+    Check out the source code here: https://github.com/SoenkeMaibach/Solaranlagen
+    '''),
+])
+        
