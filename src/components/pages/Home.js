bdff5e6757870010dc8e4e24d352fdd97e3cf873
WebDevelopmentDuesseldorf <WebDevelopmentDuesseldorf@gmail.com>
2021-09-24 18:39:22 +0200

    New Files added pages/modules/Functions.js. Home.js Page new content with Functions.js Module and 3 Cards. Some Ipsum Lorem. Design Color Palette for Nature Feeling Green and Blue for Efficiency  22577A 38A3A5 57CC99 80ED99 C7F9CC 00A8E8.  New classes for APP.CSS underline & sub-underline & functionsModule.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,92 @@
 import React from 'react';
 import SliderHome from '../inc/SliderHome'
+import Functions from './modules/Functions'
 
 
-class Home extends React.Component{
-    render(){
-        return( 
+class Home extends React.Component {
+    render() {
+        return (
             <div>
-                <h2>Home Page</h2>
-                <SliderHome />
-                <button className="btn btn-primary">Bootstrap Button Sample</button>
+                <main>
+                    
+                    <SliderHome />
+                    <article className="py-5">
+                        <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <h2>Solarpanels</h2>
+                                <div className="underline mb-3"></div>
+                                <p>
+                                    Sunt id magna et labore culpa non ut commodo sint. Duis ex incididunt nostrud pariatur qui. Nostrud veniam elit voluptate occaecat ex eu Lorem ex nisi. Enim voluptate quis culpa duis magna ad eiusmod. Nisi labore Lorem exercitation ad Lorem ad elit eu mollit. Anim nulla Lorem esse mollit qui sint ad.
+                                </p>
+                                <p>
+                                Sunt id magna et labore culpa non ut commodo sint. Duis ex incididunt nostrud pariatur qui. Nostrud veniam elit voluptate occaecat ex eu Lorem ex nisi. Enim voluptate quis culpa duis magna ad eiusmod. Nisi labore Lorem exercitation ad Lorem ad elit eu mollit. Anim nulla Lorem esse mollit qui sint ad.
+                            </p>
+                                
+                                
+                                
+                                </div>
+                            </div>
+                        </div>
+                        </article>
+                        <Functions />
+
+                        <section className="py-5">
+                        <div className="container">
+                            <div className="row">
+                            <div className="col-md-12">
+                                <h3>Informations about Renewable Energy</h3>
+                                <div className="underline mb-4"></div>
+                            </div>
+                          
+                           
+                            <div className="col-md-4">
+                            <div className="card"> 
+                                <div className="card-body">
+                                
+                                    <h4>The Mission</h4>
+                                    <div className="mb-3 sub-underline"></div>
+                                    <p>
+                                        Consequat laborum id nisi laboris ex in fugiat aute. Sit nostrud culpa ut magna ipsum deserunt duis. Ut labore voluptate consectetur nulla elit ullamco deserunt fugiat sint occaecat eu. Occaecat Lorem aliquip enim cupidatat commodo esse occaecat nisi anim id ad in nisi consectetur. Amet Lorem exercitation ullamco exercitation Lorem sunt occaecat. Fugiat mollit sint voluptate fugiat dolore cupidatat cillum nulla proident cupidatat voluptate enim velit ullamco. Velit non proident enim dolor esse eiusmod culpa cillum anim id occaecat dolor.
+                                    </p>
+                                 </div>
+                                </div>                                                                                           
+                                </div>
+                                <div className="col-md-4">
+                                <div className="card"> 
+                                <div className="card-body">
+                                
+                                    <h4>The Mission</h4>
+                                    <div className="mb-3 sub-underline"></div>
+                                    <p>
+                                        Consequat laborum id nisi laboris ex in fugiat aute. Sit nostrud culpa ut magna ipsum deserunt duis. Ut labore voluptate consectetur nulla elit ullamco deserunt fugiat sint occaecat eu. Occaecat Lorem aliquip enim cupidatat commodo esse occaecat nisi anim id ad in nisi consectetur. Amet Lorem exercitation ullamco exercitation Lorem sunt occaecat. Fugiat mollit sint voluptate fugiat dolore cupidatat cillum nulla proident cupidatat voluptate enim velit ullamco. Velit non proident enim dolor esse eiusmod culpa cillum anim id occaecat dolor.
+                                    </p>
+                                    </div>
+                                                                                                                        
+                                </div>
+                                </div>
+                                <div className="col-md-4">
+                                <div className="card"> 
+                                <div className="card-body">
+                                
+                                    <h4>The Mission</h4>
+                                    <div className="mb-3 sub-underline"></div> 
+                                    <p>
+                                        Consequat laborum id nisi laboris ex in fugiat aute. Sit nostrud culpa ut magna ipsum deserunt duis. Ut labore voluptate consectetur nulla elit ullamco deserunt fugiat sint occaecat eu. Occaecat Lorem aliquip enim cupidatat commodo esse occaecat nisi anim id ad in nisi consectetur. Amet Lorem exercitation ullamco exercitation Lorem sunt occaecat. Fugiat mollit sint voluptate fugiat dolore cupidatat cillum nulla proident cupidatat voluptate enim velit ullamco. Velit non proident enim dolor esse eiusmod culpa cillum anim id occaecat dolor.
+                                    </p>
+                                    </div>
+                                </div>                                                                                         
+                                </div>
+                            
+                            
+                            </div>
+                        </div>
+                    </section>
+
+
+                    
+                    
+                </main>
             </div>
         );
     }
---
+++ src/components/pages/modules/Functions.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+function Functions(){ 
+    return (
+        <section className="functionsModule py-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h3>The Mission</h3>
+                        <div className="sub-underline mb-2"></div>
+                        <p>
+                            Consequat laborum id nisi laboris ex in fugiat aute. Sit nostrud culpa ut magna ipsum deserunt duis. Ut labore voluptate consectetur nulla elit ullamco deserunt fugiat sint occaecat eu. Occaecat Lorem aliquip enim cupidatat commodo esse occaecat nisi anim id ad in nisi consectetur. Amet Lorem exercitation ullamco exercitation Lorem sunt occaecat. Fugiat mollit sint voluptate fugiat dolore cupidatat cillum nulla proident cupidatat voluptate enim velit ullamco. Velit non proident enim dolor esse eiusmod culpa cillum anim id occaecat dolor.
+                        </p>
+                    </div>
+                    <div className="col-md-6">
+                        <h3>Where to Begin?</h3>
+                        <div className="sub-underline mb-2"></div>
+                        <p>
+                            Consequat laborum id nisi laboris ex in fugiat aute. Sit nostrud culpa ut magna ipsum deserunt duis. Ut labore voluptate consectetur nulla elit ullamco deserunt fugiat sint occaecat eu. Occaecat Lorem aliquip enim cupidatat commodo esse occaecat nisi anim id ad in nisi consectetur. Amet Lorem exercitation ullamco exercitation Lorem sunt occaecat. Fugiat mollit sint voluptate fugiat dolore cupidatat cillum nulla proident cupidatat voluptate enim velit ullamco. Velit non proident enim dolor esse eiusmod culpa cillum anim id occaecat dolor.
+                        </p>
+                    </div>
+
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Functions
