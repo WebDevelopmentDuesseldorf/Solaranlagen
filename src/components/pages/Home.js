e9e442d8abd8cd8f32dfa114159cac564795c8ed
WebDevelopmentDuesseldorf <WebDevelopmentDuesseldorf@gmail.com>
2021-09-24 04:08:16 +0200

    added sass for swipe.js. Working on inc/SliderHome.js for slider in Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import SliderHome from '../inc/SliderHome'
+
 
 class Home extends React.Component{
     render(){
         return( 
             <div>
                 <h2>Home Page</h2>
+                <SliderHome />
                 <button class="btn btn-primary">Bootstrap Button Sample</button>
             </div>
         );
